02a26d99ec59a79efc4e9d55552e4c9ded7235b0
CarlosZelayaTec <czelayabaca@gmail.com>
2023-01-26 19:53:35 -0600

    Login

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,21 +1,24 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from "react-native";
+import React from "react";
+
+import { Input } from "../Components/Login/index";
 
 const Login = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Pantalla de Login</Text>
-      <Button title='Iniciar Sesión' onPress={() => navigation.push('Home')} />
+      <Input placeholder="Correo electronico" />
+      <Input placeholder="Contraseña" />
+      <Button title="Iniciar Sesión" onPress={() => navigation.push("Home")} />
     </View>
-  )
-}
+  );
+};
 
 export default Login;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
